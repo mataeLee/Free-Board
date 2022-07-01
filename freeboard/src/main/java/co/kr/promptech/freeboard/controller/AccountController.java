ca6dc39a45a6fa1f67d07510ea05abae1bdd7212
AuthorName <tyleeg0901@gmail.com>
2022-07-01 18:10:34 +0900

    feature : 회원 인증 폼 valid 체크 기능 구현
    - add validation dependency
    - account controller validation check
    - login, signup form valid check
    - add _csrf token

--- freeboard/src/main/java/co/kr/promptech/freeboard/controller/AccountController.java
+++ freeboard/src/main/java/co/kr/promptech/freeboard/controller/AccountController.java
@@ -4,14 +4,14 @@ import co.kr.promptech.freeboard.dto.AccountDTO;
 import co.kr.promptech.freeboard.model.Account;
 import co.kr.promptech.freeboard.service.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,12 +24,20 @@ public class AccountController {
     }
 
     @GetMapping("/signup")
-    public String signupForm(){
+    public String signupForm(Model model){
+        model.addAttribute("accountDTO", new AccountDTO());
         return "pages/accounts/new";
     }
 
     @PostMapping("/signup")
-    public String post(AccountDTO accountDTO){
+    public String post(@Validated AccountDTO accountDTO, BindingResult result){
+        Account account = accountService.findAccountByUsername(accountDTO.getUsername());
+        if(account != null){
+            result.rejectValue("username", "error.username", "Username already in use");
+        }
+        if(result.hasErrors()){
+            return "pages/accounts/new";
+        }
         accountService.save(accountDTO);
         return "redirect:/login";
     }
@@ -37,7 +45,6 @@ public class AccountController {
     @GetMapping("/accounts")
     public String show(Model model, Authentication authentication){
         String username = authentication.getName();
-
 
         return "pages/accounts/show";
     }
--- freeboard/src/main/java/co/kr/promptech/freeboard/dto/AccountDTO.java
+++ freeboard/src/main/java/co/kr/promptech/freeboard/dto/AccountDTO.java
@@ -2,13 +2,25 @@ package co.kr.promptech.freeboard.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AccountDTO {
+    @NotBlank(message = "Username required")
+    @Size(min = 5, max = 15)
     private String username;
+    @NotBlank(message = "Password required")
+    @Size(min = 3, max = 15)
     private String password;
+    @NotBlank(message = "Email required")
+    @Email()
     private String email;
     @Builder
     public AccountDTO(String username, String password, String email){
