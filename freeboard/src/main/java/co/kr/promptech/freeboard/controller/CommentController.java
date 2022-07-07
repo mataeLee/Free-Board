149598c611c0c124c62a4a2d8c8b3a843b3f686d
이태윤 <tyleeg0901@gmail.com>
2022-07-07 17:08:29 +0900

    feature : 게시글 안에서 댓글 삭제 기능 추가, 삭제 요청 페이지 위치에 따른 target url 설정 추가

--- freeboard/src/main/java/co/kr/promptech/freeboard/controller/CommentController.java
+++ freeboard/src/main/java/co/kr/promptech/freeboard/controller/CommentController.java
@@ -1,21 +1,30 @@
 package co.kr.promptech.freeboard.controller;
 
+import co.kr.promptech.freeboard.dto.ArticleDetailDTO;
 import co.kr.promptech.freeboard.dto.CommentDTO;
 import co.kr.promptech.freeboard.model.Account;
 import co.kr.promptech.freeboard.model.Article;
 import co.kr.promptech.freeboard.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/comments")
 public class CommentController {
-
     private final CommentService commentService;
 
     @PostMapping("/{articleId}")
@@ -26,9 +35,13 @@ public class CommentController {
         return "redirect:/articles/"+ commentDTO.getArticleId();
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable Long id){
-        commentService.delete(id);
-        return "redirect:/accounts/comments";
+    @DeleteMapping("/{id}/{articleId}")
+    public String delete(@PathVariable Map<String, String> pathVarsMap){
+        String targetUrl = "/accounts/comments";
+        if(pathVarsMap.containsKey("articleId") && Long.parseLong(pathVarsMap.get("articleId")) > 0){
+            targetUrl = "/articles/" + pathVarsMap.get("articleId");
+        }
+        commentService.delete(Long.parseLong(pathVarsMap.get("id")));
+        return "redirect:" + targetUrl;
     }
 }
